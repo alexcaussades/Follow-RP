61f4d69ce008addefd60e89cbd8bf88a4965a753
alexcaussades <alexcaussades@gmail.com>
2022-07-08 12:20:40 +0200

    Feat/note (#3)
    * Feacture Readme
    
    * update Readme
    
    * mod mineur
    
    * update readme
    
    * change mineur

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ except (FileNotFoundError):
     print("Creation du fichier utilisateur !")
     creat_name = input("Votre nom RP ? : ")
     creat_last_name = input("Votre prénom RP ? : ")
-    config_creat.creat_config_file(creat_name, creat_last_name)
     print("Votre fichier est configurer relancer le logiciel")
-
+    config_creat.creat_config_file(creat_name, creat_last_name)
+    
 print("Bienvenue sur Follow RP")
 print("Besoin d'aide taper: help")
 
 version.versions_serach()
+
+
+
 
 i = 'rr'
 
