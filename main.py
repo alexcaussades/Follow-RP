4119a182ab50181cd6a03315da98874a8b3b89a3
alexcaussades <alexcaussades@gmail.com>
2022-07-11 11:01:42 +0200

    change architecture

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ from datetime import date
 import version
 today = date.today()
 
+class bcolors:
+    OK = '\033[92m' #GREEN
+    WARNING = '\033[93m' #YELLOW
+    FAIL = '\033[91m' #RED
+    RESET = '\033[0m' #RESET COLOR
+
+
 
 try:
     with open('assets/config.json') as mon_fichier:
@@ -40,28 +47,40 @@ try:
         a = a.split()
 
         if (a[0] == "creat"):
-            req = a[1]
-            prenom = input("Qu'elle est son prenom ? ")
-            age = input("Qu'elle est son age ? ")
-            ville = input("Qu'elle sa ville ? (Si pas disponible (n)) ")
-            function = input("Qu'elle est sa fonction ? (doc, citoyen, cherif, autre) ")
-            sr.creatplayer(req, age, prenom, ville, function)
+            try:
+                req = a[1]
+                prenom = input("Qu'elle est son prenom ? ")
+                age = input("Qu'elle est son age ? ")
+                ville = input("Qu'elle sa ville ? (Si pas disponible (n)) ")
+                function = input("Qu'elle est sa fonction ? (doc, citoyen, cherif, autre) ")
+                sr.creatplayer(req, age, prenom, ville, function)
+            except:
+                print("Manque information player")
         
 
         if (a[0] == "sr"):
-            req = a[1]
-            sr.player(req)
+            try:
+                req = a[1]
+                sr.player(req)
+            except:
+                print("Manque information player")
             
         if (a[0] == "edit"):
-            req = a[1]
-            information = input("votre nouvelle information ? ")
-            sr.editplayer(req, information)
+            try:
+                req = a[1]
+                information = input("votre nouvelle information ? ")
+                sr.editplayer(req, information)
+            except: 
+                print("Manque information player")
 
         if (a[0] == 'help'):
             help.help_funtion()
+
+        if (a[0] == 'version'):
+            version.numero_version()
             
         if (a[0] == "exit"):
             quit()
     
-except (KeyboardInterrupt):
+except (KeyboardInterrupt): 
     print(" Fermeture du logiciel")
